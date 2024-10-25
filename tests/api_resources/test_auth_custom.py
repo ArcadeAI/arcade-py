bb03f751a35743db187c21a8f9ff504a5542a70f
Nate Barbettini <nate@arcade-ai.com>
2024-10-24 17:22:16 -0700

    More tests

--- tests/api_resources/test_auth_custom.py
+++ tests/api_resources/test_auth_custom.py
@@ -1,14 +1,59 @@
-from unittest.mock import AsyncMock
+from typing import List, Union, Optional
+from unittest.mock import Mock, AsyncMock
 
 import pytest
 
-from arcadepy._client import AsyncArcade
-from arcadepy.resources.auth import AsyncAuthResource
+from arcadepy._types import NOT_GIVEN, NotGiven
+from arcadepy._client import Arcade, AsyncArcade
+from arcadepy.resources.auth import AuthResource, AsyncAuthResource
 from arcadepy.types.shared.authorization_response import AuthorizationResponse
 
 
+@pytest.mark.parametrize(
+    "scopes, expected_scopes",
+    [
+        (["scope1"], "scope1"),
+        (["scope1", "scope2"], "scope1 scope2"),
+        (None, NOT_GIVEN),
+    ],
+)
+def test_wait_for_completion_calls_status_from_auth_response(
+    scopes: Optional[List[str]], expected_scopes: Union[str, NotGiven]
+) -> None:
+    client = Arcade(api_key="test")
+    auth = AuthResource(client)
+    auth.status = Mock(return_value=AuthorizationResponse(status="completed"))  # type: ignore
+
+    auth_response_or_id = AuthorizationResponse(status="pending", authorization_id="auth_id123", scopes=scopes)
+
+    auth.wait_for_completion(auth_response_or_id)
+
+    auth.status.assert_called_with(
+        authorization_id="auth_id123",
+        scopes=expected_scopes,
+        wait=45,
+    )
+
+
+def test_wait_for_completion_calls_status_with_auth_id() -> None:
+    client = Arcade(api_key="test")
+    auth = AuthResource(client)
+    auth.status = Mock(return_value=AuthorizationResponse(status="completed"))  # type: ignore
+
+    auth_response_or_id = "auth_id123"
+    scopes = ["scope1", "scope2"]
+
+    auth.wait_for_completion(auth_response_or_id, scopes)
+
+    auth.status.assert_called_with(
+        authorization_id="auth_id123",
+        scopes="scope1 scope2",
+        wait=45,
+    )
+
+
 @pytest.mark.asyncio
-async def test_wait_for_completion_calls_status_from_auth_response() -> None:
+async def test_async_wait_for_completion_calls_status_from_auth_response() -> None:
     client = AsyncArcade(api_key="test")
     auth = AsyncAuthResource(client)
     auth.status = AsyncMock(return_value=AuthorizationResponse(status="completed"))  # type: ignore
@@ -27,10 +72,10 @@ async def test_wait_for_completion_calls_status_from_auth_response() -> None:
 
 
 @pytest.mark.asyncio
-async def test_wait_for_completion_calls_status_with_auth_id() -> None:
+async def test_async_wait_for_completion_calls_status_with_auth_id() -> None:
     client = AsyncArcade(api_key="test")
     auth = AsyncAuthResource(client)
-    auth.status = AsyncMock(return_value=AuthorizationResponse(status="completed"))
+    auth.status = AsyncMock(return_value=AuthorizationResponse(status="completed"))  # type: ignore
 
     auth_response_or_id = "auth_id"
     scopes = ["scope1", "scope2"]
